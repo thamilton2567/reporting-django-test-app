df79191d8e9b09f22f48c85bfb7a634390b970ef
thamilton2567 <thamilton2567@gmail.com>
2023-03-10 09:30:40 -0500

    complete task 2 - expand row of customer

--- reporting/views.py
+++ reporting/views.py
@@ -1,27 +1,34 @@
 from django.shortcuts import render
-from .models import Business, Job
-from django.db.models import Sum, Exists, OuterRef, Q
+from .models import Business, Job, Invoice
+from django.db.models import Sum, Exists, OuterRef
 from .forms import FilterForm
 
 # Create your views here.
 def index(request):
   
   customers = Business.objects.filter(Exists(Job.objects.filter(business=OuterRef('id')))) \
-              .annotate(total_job_line_item_amount=Sum("jobs__lineitem__amount")) \
-              .annotate(total_job_line_item_amount_remaining_to_be_invoiced=(Sum("jobs__lineitem__amount")-Sum('jobs__lineitem__invoiceditem__amount'))) \
-              .annotate(total_invoice_line_item_amount=Sum('jobs__lineitem__invoiceditem__amount')) \
-              .order_by('-total_job_line_item_amount_remaining_to_be_invoiced', 'name')
+              .annotate(total_job_line_item_amount=Sum("jobs__lineitem__amount"),
+                total_job_line_item_amount_remaining_to_be_invoiced=(Sum("jobs__lineitem__amount")-Sum('jobs__lineitem__invoiceditem__amount')),
+                total_invoice_line_item_amount=Sum('jobs__lineitem__invoiceditem__amount')
+              ).order_by('-total_job_line_item_amount_remaining_to_be_invoiced', 'name')
               
   if request.method == "POST":
-      #Get the posted form
-      MyDataForm = FilterForm(request.POST)
-      
-      if MyDataForm.is_valid():
-        min = MyDataForm.cleaned_data['min']
-        max = MyDataForm.cleaned_data['max']
-        if max > min:
-          customers = customers.filter(total_job_line_item_amount_remaining_to_be_invoiced__gt=min, total_job_line_item_amount_remaining_to_be_invoiced__lt=max)
+    #Get the posted form
+    MyDataForm = FilterForm(request.POST)
+    
+    if MyDataForm.is_valid():
+      min = MyDataForm.cleaned_data['min']
+      max = MyDataForm.cleaned_data['max']
+      if max > min:
+        customers = customers.filter(total_job_line_item_amount_remaining_to_be_invoiced__gt=min, total_job_line_item_amount_remaining_to_be_invoiced__lt=max)
   
+  for customer in customers:
+    customer_all_invoiced_items = Invoice.objects.filter(lineitem__self_item__job__business=customer).distinct()
+    customer.invoice_lists = customer_all_invoiced_items    
+  
+    payments = customer.payments_sent.all().values("payment_type", "reference").annotate(amount_sum=Sum('amount')).order_by('payment_type', 'reference')
+    customer.payment_lists = payments
+
   context = {
     "customers": customers
   }

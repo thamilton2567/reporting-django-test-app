a485b76355c7641bd30f63a5a8d597fc622538f5
thamilton2567 <thamilton2567@gmail.com>
2023-03-10 01:10:55 -0500

    fix wrong part of task 1 - customer lists and total_invoice_line_item_amount

--- reporting/views.py
+++ reporting/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from .models import Business
-from django.db.models import Sum
+from .models import Business, Job
+from django.db.models import Sum, Exists, OuterRef, Q
 from .forms import FilterForm
 
 # Create your views here.
 def index(request):
   
-  customers = Business.objects.all() \
+  customers = Business.objects.filter(Exists(Job.objects.filter(business=OuterRef('id')))) \
               .annotate(total_job_line_item_amount=Sum("jobs__lineitem__amount")) \
               .annotate(total_job_line_item_amount_remaining_to_be_invoiced=(Sum("jobs__lineitem__amount")-Sum('jobs__lineitem__invoiceditem__amount'))) \
-              .annotate(total_invoice_line_item_amount=Sum("invoices__lineitem__amount")) \
+              .annotate(total_invoice_line_item_amount=Sum('jobs__lineitem__invoiceditem__amount')) \
               .order_by('-total_job_line_item_amount_remaining_to_be_invoiced', 'name')
               
   if request.method == "POST":
